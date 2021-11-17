01961e5176b1f1dfbd87428425845a872ca269ea
takoyaki-3 <mail@takoyaki3.com>
2021-11-17 16:06:17 +0900

    fix

--- edge_timetable/converter.go
+++ edge_timetable/converter.go
@@ -2,6 +2,10 @@ package edgetimetable
 
 import (
 	"sort"
+	"fmt"
+	"path"
+
+	csvtag "github.com/artonge/go-csv-tag/v2"
 
 	. "github.com/takoyaki-3/go-gtfs"
 )
@@ -57,4 +61,29 @@ func GTFS2TimeTableEdges(g *GTFS)(et *EdgeTimetable){
 	}
 
 	return et
+}
+
+func Dump(edgeTimetable *EdgeTimetable, dirPath string, filter map[string]bool) error {
+
+	files := map[string]interface{}{
+		"ptedges.csv":        edgeTimetable.Edges,
+		"pteproperties.csv":  edgeTimetable.Properties,
+		"stops.csv":					edgeTimetable.Stops,
+	}
+	
+	for file, src := range files {
+		if filter != nil && !filter[file[:len(file)-4]] {
+			continue
+		}
+		if src == nil {
+			continue
+		}
+		filePath := path.Join(dirPath, file)
+
+		err := csvtag.DumpToFile(src, filePath)
+		if err != nil {
+			return fmt.Errorf("Error dumping file %v: %v", file, err)
+		}
+	}
+	return nil
 }
